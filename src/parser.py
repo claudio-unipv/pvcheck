3f7cab67332fd63e81fd7dd53396934fe1563f3e
Claudio Cusano <claudio.cusano@unipv.it>
2017-03-28 10:53:22 +0200

    Fixed a bug in the parser

--- src/parser.py
+++ src/parser.py
@@ -4,7 +4,23 @@ import re
 import testdata
 
 
-_RE_HEADER = re.compile(r"\s*\[\s*(?P<tag>[._a-zA-Z][._\w]*)\s*\]\s*")
+# Any character is allowed before '[', otherwise the header will be
+# missed after a scanf (or other input) where the newline is taken
+# from the stdin and is not part of the stdout.  In practice, the
+# condition to deal with is 
+#
+# ...
+# printf("Enter n: ");
+# scanf("%d", &n);
+# printf("[SECTION]\n%d\n", n);
+# ...
+#
+# whose output is:
+#
+# Enter n:  [SECTION]
+# 42
+#
+_RE_HEADER = re.compile(r"[^[]*\[\s*(?P<tag>[._a-zA-Z][._\w]*)\s*\]\s*")
 
 
 def parse_sections(f):
