17b9d85075b2ae67de375afd7b43a964dddaa989
dbarattini <daniel.barattini@outlook.com>
2017-04-18 18:31:38 +0200

    Creata classe argparser

--- src/app.py
+++ src/app.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 import sys
-import argparse
+from argparser import ArgParser
 import os
 import math
 import pvcheck
@@ -22,32 +22,8 @@ _DEFAULT_LOG_FILE = os.path.expanduser("~/.pvcheck.log")
 
 def parse_options():
     """Parse the command line."""
-    argparser = argparse.ArgumentParser(description=_("Run tests to verify the correctness of a program."), add_help=False)
-    argparser.add_argument("-h", "--help", help=_("print this message and exit."), action='help')
-    argparser.add_argument("-c", "--config", help=_("uses the specified configuration file."), nargs='?', const='',
-                           default='')
-    argparser.add_argument("-t", "--timeout", help=_("set how many seconds it should be waited for the termination of the program.  The default is 10 seconds."),
-                           nargs='?', const='10', default='10', type=float)
-    argparser.add_argument("-v", "--verbosity", help=_("set the verbosity level, where the level must be an integer between 0 (minimum) and 4 (maximum). The default value is 3."),
-                           nargs='?', const='3', default='3', type=int)
-    argparser.add_argument("-m", "--max_errors", help=_("reports up to N errors per section (default 4)."), nargs='?',
-                           const='4', default='4', type=int)
-    argparser.add_argument("-C", "--color", help=_("enable or disable colored output (default AUTO)."), nargs='?',
-                           const='AUTO', default='AUTO')
-    argparser.add_argument("-V", "--valgrind", help=_("use Valgrind (if installed) to check memory usage."),
-                           action='store_true')
-    argparser.add_argument("-f", "--format", help=_("select the output type."), nargs='?', const='resume',
-                           default='resume')
-    argparser.add_argument("-l", "--log", help=_("specify the name of the file used for logging.  The default is ~/.pvcheck.log."),
-                           nargs='?', const=_DEFAULT_LOG_FILE, default=_DEFAULT_LOG_FILE)
-    argparser.add_argument("-ls", "--list", help=_("list all the available tests."), action='store_true')
-    argparser.add_argument("-r", "--run", help=_("run only the selected test."), nargs="?", type=int)
-    argparser.add_argument("-e", "--export", help=_("export in a file the input arguments from the selected test."),
-                           nargs="?", type=int)
-    argparser.add_argument("test_file", help=_("file containing the tests to be performed."))
-    argparser.add_argument("program", help=_("program to be tested."))
-    argparser.add_argument("program_arguments", help="any arguments of the program to be tested.", nargs='*')
-
+    argparser = ArgParser(description=_("Run tests to verify the correctness of a program."), add_help=False)
+    argparser.initialize()
     args = argparser.parse_args()
 
     verbosity = args.verbosity
--- src/i18n.py
+++ src/i18n.py
@@ -99,7 +99,7 @@ Use 'list' to list all the available tests. $ Utilizza 'list' per vedere tutti i
 Usage: list testfile $ Utilizzo: list fileditest
 "Usage: N run[or export] testfile executable" $ Utilizzo: N run fileditest eseguibile
 Error: Can't export test number %d. $ Errore: Impossibile esportare il test numero %d.
-file containing the tests to be performed. $ file contenente i test da eseguire.
+file containing the tests to be performed (default pvcheck.test). $ file contenente i test da eseguire (default pvcheck.test).
 program to be tested. $ programma da testare.
 any arguments of the program to be tested. $ eventuali argomenti del programma da testare.
 """
