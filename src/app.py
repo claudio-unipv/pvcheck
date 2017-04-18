aee99e190628315d2df243bae91f8d29e9e9b883
dbarattini <daniel.barattini@outlook.com>
2017-04-18 18:41:20 +0200

    Sostituita la classe argparser con un inizializzazione

--- src/app.py
+++ src/app.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 import sys
-from argparser import ArgParser
+import argparse
 import os
 import math
 import pvcheck
@@ -22,8 +22,7 @@ _DEFAULT_LOG_FILE = os.path.expanduser("~/.pvcheck.log")
 
 def parse_options():
     """Parse the command line."""
-    argparser = ArgParser(description=_("Run tests to verify the correctness of a program."), add_help=False)
-    argparser.initialize()
+    argparser = _initialized_argparser()
     args = argparser.parse_args()
 
     verbosity = args.verbosity
@@ -71,6 +70,38 @@ def parse_options():
                 format=format, logfile=logfile, list=list, run=run, export=export)
 
     return (args, opts)
+
+
+def _initialized_argparser():
+    argparser = argparse.ArgumentParser(description=_("Run tests to verify the correctness of a program."),
+                                        add_help=False)
+    argparser.add_argument("-h", "--help", help=_("print this message and exit."), action='help')
+    argparser.add_argument("-c", "--config", help=_("uses the specified configuration file."), nargs='?', const='',
+                           default='')
+    argparser.add_argument("-t", "--timeout", help=_("set how many seconds it should be waited for the termination of "
+                           "the program.  The default is 10 seconds."), nargs='?', const='10', default='10', type=float)
+    argparser.add_argument("-v", "--verbosity", help=_("set the verbosity level, where the level must be an integer "
+                           "between 0 (minimum) and 4 (maximum). The default value is 3."), nargs='?', const='3',
+                           default='3', type=int)
+    argparser.add_argument("-m", "--max_errors", help=_("reports up to N errors per section (default 4)."), nargs='?',
+                           const='4', default='4', type=int)
+    argparser.add_argument("-C", "--color", help=_("enable or disable colored output (default AUTO)."), nargs='?',
+                           const='AUTO', default='AUTO')
+    argparser.add_argument("-V", "--valgrind", help=_("use Valgrind (if installed) to check memory usage."),
+                           action='store_true')
+    argparser.add_argument("-f", "--format", help=_("select the output type."), nargs='?', const='resume',
+                           default='resume')
+    argparser.add_argument("-l", "--log", help=_("specify the name of the file used for logging.  The default is "
+                           "~/.pvcheck.log."), nargs='?', const=_DEFAULT_LOG_FILE, default=_DEFAULT_LOG_FILE)
+    argparser.add_argument("-ls", "--list", help=_("list all the available tests."), action='store_true')
+    argparser.add_argument("-r", "--run", help=_("run only the selected test."), nargs="?", type=int)
+    argparser.add_argument("-e", "--export", help=_("export in a file the input arguments from the selected test."),
+                           nargs="?", type=int)
+    argparser.add_argument("test_file", help=_("file containing the tests to be performed (default pvcheck.test)."),
+                           default="pvcheck.test")
+    argparser.add_argument("program", help=_("program to be tested."))
+    argparser.add_argument("program_arguments", help="any arguments of the program to be tested.", nargs='*')
+    return argparser
 
 
 def parse_file(filename):
