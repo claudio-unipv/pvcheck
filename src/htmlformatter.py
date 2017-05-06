4cc26c021bfee17ae4d3f3db2eb49827fe45e350
dbarattini <daniel.barattini@outlook.com>
2017-05-06 18:55:09 +0200

    Implementato CSS per la tabella

--- src/htmlformatter.py
+++ src/htmlformatter.py
@@ -10,7 +10,43 @@ class HTMLFormatter(JSONFormatter):
     <head>
         <meta charset="utf-8">
         <title>PvCheck Result</title>
-        <link href="minimal-table.css" rel="stylesheet" type="text/css">
+        <style rel="stylesheet" type="text/css">
+            html {
+              font-family: sans-serif;
+            }
+    
+            table {
+              border-collapse: collapse;
+              border: 2px solid rgb(200,200,200);
+              letter-spacing: 1px;
+              font-size: 0.8rem;
+            }
+    
+            td, th {
+              border: 1px solid rgb(190,190,190);
+              padding: 10px 20px;
+            }
+    
+            th {
+              background-color: rgb(235,235,235);
+            }
+    
+            td {
+              text-align: center;
+            }
+    
+            tr:nth-child(even) td {
+              background-color: rgb(250,250,250);
+            }
+    
+            tr:nth-child(odd) td {
+              background-color: rgb(245,245,245);
+            }
+    
+            caption {
+              padding: 10px;
+            }
+        </style>
     </head>
     <body>
         <table align="center">
