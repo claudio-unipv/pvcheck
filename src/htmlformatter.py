73c1462c114b21a38d844e0976a8cb72318b6887
dbarattini <daniel.barattini@outlook.com>
2017-05-22 00:45:20 +0200

    Refactoring

--- src/htmlformatter.py
+++ src/htmlformatter.py
@@ -13,6 +13,7 @@ total_summary = {"ok": 0, "warning": 0, "error": 0}
 
 
 class HTMLFormatter(JSONFormatter):
+
     def end_session(self):
 
         header = self._tests_table_header_builder()
@@ -88,11 +89,6 @@ class HTMLFormatter(JSONFormatter):
         print("        </table>")
 
     def _tests_table_header_builder(self):
-        """Build the table header.
-
-        If there is only a test omits the header 'TEST'.
-
-        """
         if len(self._tests) > 1:
             header = ["TEST"]
         else:
@@ -119,28 +115,9 @@ class HTMLFormatter(JSONFormatter):
             first_element_index = 0
         for test in self._tests:
             row = self._test_table_row_builder(test, tests_table_header)
-            print("            <tr>")
-            if print_test_name:
-                print('                <td><a href="#{}">{}</a></td>'.format(row[0].translate(_trantab),
-                                                                             row[0].translate(_trantab)))
-            for element in row[first_element_index:]:
-                if element == "ok":
-                    color = "green"
-                elif element == "error":
-                    color = "red"
-                elif element == "missing":
-                    color = "orange"
-                else:
-                    color = "black"
-                print('                <td><font color="{}">{}</font></td>'.format(color, element.translate(_trantab)))
-            print("            </tr>")
+            self._print_test_table_row(print_test_name, first_element_index, row)
 
     def _test_table_row_builder(self, test, header):
-        """Build a row.
-
-        If there is only a test omits the test's name.
-
-        """
         if len(self._tests) > 1:
             row = [test["title"]]
         else:
@@ -153,90 +130,131 @@ class HTMLFormatter(JSONFormatter):
                     row.append("")
         return row
 
+    def _print_test_table_row(self, print_test_name, first_element_index, row):
+        print("            <tr>")
+        if print_test_name:
+            print('                <td><a href="#{}">{}</a></td>'.format(row[0].translate(_trantab),
+                                                                         row[0].translate(_trantab)))
+        for element in row[first_element_index:]:
+            self._print_section_status(element)
+        print("            </tr>")
+
+    @staticmethod
+    def _print_section_status(section):
+        if section == "ok":
+            color = "green"
+        elif section == "error":
+            color = "red"
+        elif section == "missing":
+            color = "orange"
+        else:
+            color = "black"
+        print('                <td><font color="{}">{}</font></td>'.format(color, section.translate(_trantab)))
+
     def print_tests_information(self):
+        """Print a section for each test containing results' information."""
         if self._tests[0]["error_message"] != "ok":
-            print("        <hr>")
-            print("        <p><font color='red'>{}</font></p>".format(self._tests[0]["error_message"]))
-            print("        <hr>")
-            print("    </body>")
-            print('</html>')
+            self._print_error_message()
             exit(0)
         header = self._tests_table_header_builder()
         for test in self._tests:
-            print("        <hr>")
-            if len(self._tests) > 1:
-                print('        <p><a name="{}"><b>TEST:</b> {}</a><br>'.format(test["title"].translate(_trantab),
-                                                                        test["title"].translate(_trantab)))
-            command_line = ""
-            for element in test["command_line"]:
-                command_line += " " + element
-            print('           <b>{}:</b> {}<br>'.format(_("COMMAND LINE"), command_line.translate(_trantab)))
-            if len(test["input_text"]) > 0:
-                print('            <b>INPUT:</b> {}<br>'.format(test["input_text"].translate(_trantab)))
-            if test["input_file_name"] is not None:
-                if test["input_file_name"] == "<temp.file>":
-                    input_file_name = _("TEMPORARY FILE")
-                else:
-                    input_file_name = test["input_file_name"]
-
-                print('            <b>{}:</b><br> {}<br>'.format(input_file_name.translate(_trantab),
-                                                          test["file_text"].translate(_trantab)))
-            for section in header:
-                if section != "TEST":
-                    if test["sections"][section]["section status"] == "ok":
-
-                        section_summary[section]["ok"] += 1
-                        total_summary["ok"] += 1
-
-                        msg = "OK"
-                        color = "green"
-                        print('            <b><font color="{}">{}: </b>{}</font><br>'.format(color, section, msg))
-                    elif test["sections"][section]["section status"] == "error":
-
-                        section_summary[section]["error"] += 1
-                        total_summary["error"] += 1
-
-                        for wrong_line in test["sections"][section]['wrong_lines']:
-                            if wrong_line[2] is None:
-                                msg = _("unexpected line '%s'") % (wrong_line[0] + 1)
-                            elif wrong_line[1] is None:
-                                msg = _("missing line (expected '%s')") % (wrong_line[2])
-                            else:
-                                msg = _("line %d is wrong  (expected '%s', got '%s')") % (wrong_line[0] + 1, wrong_line[2],
-                                                                                          wrong_line[1])
-                            color = "red"
-                            print('            <b><font color="{}">{}: </b>{}</font><br>'.format(color, section, msg))
-                    else:
-
-                        section_summary[section]["warning"] += 1
-                        total_summary["warning"] += 1
-
-                        msg = _("missing section")
-                        color = "orange"
-                        print('            <b><font color="{}">{}: </b>{}</font><br>'.format(color, section, msg))
-
-            print('        </p>')
+            self._print_test_information(test, header)
         print("        <hr>")
 
+    def _print_error_message(self):
+        print("        <hr>")
+        print("        <p><font color='red'>{}</font></p>".format(self._tests[0]["error_message"]))
+        print("        <hr>")
+        print("    </body>")
+        print('</html>')
+
+    def _print_test_information(self, test, sections):
+        print("        <hr>")
+        if len(self._tests) > 1:
+            self._print_test_name(test)
+        self._print_command_line(test)
+        if len(test["input_text"]) > 0:
+            self._print_input_text(test)
+        if test["input_file_name"] is not None:
+            self._print_input_file_name(test)
+        for section in sections:
+            if section != "TEST":
+                self._print_section_status_message(test, section)
+        print('        </p>')
+
+    @staticmethod
+    def _print_test_name(test):
+        print('        <p><a name="{}"><b>TEST:</b> {}</a><br>'.format(test["title"].translate(_trantab),
+                                                                       test["title"].translate(_trantab)))
+
+    @staticmethod
+    def _print_command_line(test):
+        command_line = ""
+        for element in test["command_line"]:
+            command_line += " " + element
+        print('           <b>{}:</b> {}<br>'.format(_("COMMAND LINE"), command_line.translate(_trantab)))
+
+    @staticmethod
+    def _print_input_text(test):
+        print('            <b>INPUT:</b> {}<br>'.format(test["input_text"].translate(_trantab)))
+
+    @staticmethod
+    def _print_input_file_name(test):
+        if test["input_file_name"] == "<temp.file>":
+            input_file_name = _("TEMPORARY FILE")
+        else:
+            input_file_name = test["input_file_name"]
+
+        print('            <b>{}:</b><br> {}<br>'.format(input_file_name.translate(_trantab),
+                                                         test["file_text"].translate(_trantab)))
+
+    def _print_section_status_message(self, test, section):
+        if test["sections"][section]["section status"] == "ok":
+            section_summary[section]["ok"] += 1
+            total_summary["ok"] += 1
+            self._print_section_ok_message(section)
+        elif test["sections"][section]["section status"] == "error":
+            section_summary[section]["error"] += 1
+            total_summary["error"] += 1
+            for wrong_line in test["sections"][section]['wrong_lines']:
+                self._print_section_error_message(wrong_line, section)
+        else:
+            section_summary[section]["warning"] += 1
+            total_summary["warning"] += 1
+            self._print_section_warning_message(section)
+
+    @staticmethod
+    def _print_section_ok_message(section):
+        msg = "OK"
+        color = "green"
+        print('            <b><font color="{}">{}: </b>{}</font><br>'.format(color, section, msg))
+
+    @staticmethod
+    def _print_section_error_message(wrong_line, section):
+        if wrong_line[2] is None:
+            msg = _("unexpected line '%s'") % (wrong_line[0] + 1)
+        elif wrong_line[1] is None:
+            msg = _("missing line (expected '%s')") % (wrong_line[2])
+        else:
+            msg = _("line %d is wrong  (expected '%s', got '%s')") % (wrong_line[0] + 1, wrong_line[2],
+                                                                      wrong_line[1])
+        color = "red"
+        print('            <b><font color="{}">{}: </b>{}</font><br>'.format(color, section, msg))
+
+    @staticmethod
+    def _print_section_warning_message(section):
+        msg = _("missing section")
+        color = "orange"
+        print('            <b><font color="{}">{}: </b>{}</font><br>'.format(color, section, msg))
+
     def print_summary_table(self):
+        """Print a test summary table."""
         self._print_summary_table_header()
         header = self._tests_table_header_builder()
         for section in header:
             if section != "TEST":
-                print("            <tr>")
-                print('                <td>{}</td>'.format(section))
-                print('                <td>{}</td>'.format(section_summary[section]["ok"]))
-                print('                <td>{}</td>'.format(section_summary[section]["warning"]))
-                print('                <td>{}</td>'.format(section_summary[section]["error"]))
-                print("            </tr>")
-
-        print("            <tr>")
-        print('                <td>{}</td>'.format(_("TOTAL")))
-        print('                <td>{}</td>'.format(total_summary["ok"]))
-        print('                <td>{}</td>'.format(total_summary["warning"]))
-        print('                <td>{}</td>'.format(total_summary["error"]))
-        print("            </tr>")
-
+                self._print_section_summary_row(section)
+        self._print_total_summary_row()
         print("        </table>")
 
     @staticmethod
@@ -249,3 +267,20 @@ class HTMLFormatter(JSONFormatter):
                 <th><font color = "red">{}</font></th>
             </tr>""".format(_("Successes"), _("Warnings"), _("Errors")))
 
+    @staticmethod
+    def _print_section_summary_row(section):
+        print("            <tr>")
+        print('                <td>{}</td>'.format(section))
+        print('                <td>{}</td>'.format(section_summary[section]["ok"]))
+        print('                <td>{}</td>'.format(section_summary[section]["warning"]))
+        print('                <td>{}</td>'.format(section_summary[section]["error"]))
+        print("            </tr>")
+
+    @staticmethod
+    def _print_total_summary_row():
+        print("            <tr>")
+        print('                <td>{}</td>'.format(_("TOTAL")))
+        print('                <td>{}</td>'.format(total_summary["ok"]))
+        print('                <td>{}</td>'.format(total_summary["warning"]))
+        print('                <td>{}</td>'.format(total_summary["error"]))
+        print("            </tr>")
